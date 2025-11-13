50e5552a956191395068c103c405861c15338852
Mohammed Aeraf <aeraf@outlook.com>
2025-11-13 19:04:48 +0530

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,72 @@
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Counter from "./Components/Counter";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import CourseList3 from "./Components/CourseList3";
 
 function App() {
   return (
-    <div className="container my-3">
-      <div className="mx-auto">
-        <Counter></Counter>
+    <Router>
+      <div className="container my-3">
+        <h1>My React Application</h1>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              FLA
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/courses"
+                  >
+                    Courses
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/courses" element={<CourseList3 />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
       </div>
-    </div>
+    </Router>
   );
 }
 
